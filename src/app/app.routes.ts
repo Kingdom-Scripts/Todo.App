7e50253c622e7043dc66a83c8b9523c11aeaec98
Mordecai Godwin <davidire71@gmail.com>
2025-07-02 10:15:35 +0100

    ADD: define application routes for AllTodo, CreateTodo, UpdateTodo, and TodoDetail components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { AllTodoComponent } from './all-todos/all-todo.component';
+import { CreateTodoComponent } from './create-todo/create-todo.component';
+import { UpdateTodoComponent } from './update-todo/update-todo.component';
+import { TodoDetailComponent } from './todo-detail/todo-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AllTodoComponent,
+  },
+  {
+    path: 'create',
+    component: CreateTodoComponent,
+  },
+  {
+    path: 'update/:id',
+    component: UpdateTodoComponent,
+  },
+  {
+    path: 'detail/:id',
+    component: TodoDetailComponent,
+  },
+];
