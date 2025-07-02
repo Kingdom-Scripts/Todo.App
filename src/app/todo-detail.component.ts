a28edd18b9630f04ec32bb915042bd89c659f1e0
Mordecai Godwin <davidire71@gmail.com>
2025-07-02 10:00:37 +0100

    ADD: create TodoDetail component with template and styles

---
+++ src/app/todo-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'all-todo-root',
+  templateUrl: './all-todo.component.html',
+  styleUrl: './all-todo.component.scss',
+})
+export class AllTodoComponent {
+  title = 'Todo Detail';
+}
