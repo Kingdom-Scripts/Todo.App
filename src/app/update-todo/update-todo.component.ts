1c60215e2842d16ba60b8b9925226b37301daf0d
Nafisat Akokhia <156707230+naphydeby@users.noreply.github.com>
2025-07-06 11:04:22 +0100

    update todo and todo detail created

--- src/app/update-todo/update-todo.component.ts
+++ src/app/update-todo/update-todo.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'update-todo-root',
+//   templateUrl: './update-todo.component.html',
+//   styleUrl: './update-todo.component.scss',
+// })
+// export class UpdateTodoComponent {
+//   title = 'Update Todo';
+// }
+
+
+
+
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormBuilder, FormGroup, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'update-todo-root',
+  standalone: true,   
+  imports: [
+    CommonModule,          
+    ReactiveFormsModule,   
+    FormsModule,           
+    RouterModule,          
+    HttpClientModule       
+  ],
   templateUrl: './update-todo.component.html',
-  styleUrl: './update-todo.component.scss',
+  styleUrls: ['./update-todo.component.scss'],
 })
-export class UpdateTodoComponent {
-  title = 'Update Todo';
+export class UpdateTodoComponent implements OnInit {
+  todoForm!: FormGroup;
+  private apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+  todoId!: string;
+
+  constructor(
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.todoId = this.route.snapshot.paramMap.get('id')!;
+    this.todoForm = this.fb.group({
+      title: [''],
+      completed: [false],
+    });
+
+    this.http.get(`${this.apiUrl}/${this.todoId}`).subscribe((data: any) => {
+      this.todoForm.patchValue({
+        title: data.title,
+        completed: data.completed,
+      });
+    });
+  }
+
+  onSubmit() {
+    console.log('Form submitted:', this.todoForm.value);
+    alert('This is a fake update for now!');
+    this.router.navigate(['/']);
+  }
 }
+
